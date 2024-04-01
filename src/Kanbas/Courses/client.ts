8d4c7443027fc00507713d76da35a920b31bab85
spoony912 <jiaoyangdu9@gmail.com>
2024-04-01 11:33:35 -0400

    env update

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 // const COURSES_API = "https://kanbas-node-server-app-xsii.onrender.com/api/courses";
 const API_BASE = process.env.REACT_APP_API_BASE;
-const COURSES_API = `${API_BASE}/courses`;
+const COURSES_API = `${API_BASE}/api/courses`;
+
 export const fetchAllCourses = async () => {
   const response = await axios.get(COURSES_API);
   return response.data;
@@ -22,7 +23,6 @@ export const createCourse = async (course: any) => {
 
 export const deleteCourse = async (id: string) => {
   const response = await axios.delete(
-    // `http://localhost:4000/api/courses/${id}`
     `${API_BASE}/api/courses/${id}`
   );
   return response.data;
@@ -32,8 +32,6 @@ export const deleteCourse = async (id: string) => {
 
 export const updateCourse = async (course: any) => {
   const response = await axios.put(
-    // `http://localhost:4000/api/courses/${course._id}`, course
-    // `${COURSES_API}/${course._id}`, course
     `${API_BASE}/api/courses/${course._id}`, course
   );
   return response.data;
